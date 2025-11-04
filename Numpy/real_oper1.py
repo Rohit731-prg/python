af2faf603d54f52e86753c448424e6d887be3169
Rohit731-prg <rohitsingha821380@gmail.com>
2025-11-04 21:59:21 +0530

    numpy real data

--- Numpy/real_oper1.py
+++ Numpy/real_oper1.py
@@ -12,3 +12,6 @@ sales_data = np.array([
 print("Sales Data shape: ", sales_data.shape)
 print("1st 3 resturent data: \n", sales_data[0:3, 1:])
 print("Total sales: \n", np.sum(sales_data[:, 1:], axis=0))
+print("Minimun sales per resturent: ", np.min(sales_data[:, 1:], axis=1))
+print("Maximum sales per year: ", np.max(sales_data[:, 1:], axis=0))
+print("Avarage sales of each resturent: ", np.average(sales_data[:, 1:], axis=1))
