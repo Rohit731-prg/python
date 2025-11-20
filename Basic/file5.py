b5df9a810db408b152bef5a9e61dda637ced9df6
Rohit731-prg <rohitsingha821380@gmail.com>
2025-11-20 10:20:42 +0530

    numpy added

---
+++ Basic/file5.py
@@ -0,0 +1,9 @@
+print('''this is text''')
+x = 10
+print(id(x))
+
+x = x + 10
+print(id(x))
+
+c = 2 + 4j
+print(type(c))
---
+++ Numpy/file10.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.arange(10)
+print("Original array: ", arr)
+print("\nSin of array: ", np.sin(arr))
+print("\nSin of array: ", np.cos(arr))
+print("\nSin of array: ", np.tan(arr))
+print("\nSin of array: ", np.sqrt(arr))
